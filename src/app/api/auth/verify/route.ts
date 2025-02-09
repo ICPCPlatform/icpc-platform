6f6844e4e6cc08b6bf6dfe2ffa11b863684ee605
Mohamed El tamawey <tammwy22@gmail.com>
2025-02-09 07:43:38 +0200

    add: /verify
    this route is use for email verification

---
+++ src/app/api/auth/verify/route.ts
@@ -0,0 +1,25 @@
+import { db } from "@/lib/db";
+import { EmailAuth } from "@/lib/db/schema/user/EmailAuth";
+import { Users } from "@/lib/db/schema/user/Users";
+import { eq } from "drizzle-orm";
+import { type NextRequest, NextResponse } from "next/server";
+import { redirect } from "next/navigation";
+
+export async function GET(request: NextRequest) {
+  const res = await db
+    .delete(EmailAuth)
+    .where(eq(EmailAuth.token, request.nextUrl.searchParams.get("token") ?? ""))
+    .returning();
+  if (res.length === 0) return redirect("/login");
+  const user = res[0];
+  const { username } = (
+    await db
+      .select({ username: Users.username })
+      .from(Users)
+      .where(eq(Users.userId, user.userId))
+      .execute()
+  )[0];
+  return redirect(
+    `/login?username=${encodeURIComponent(username)}`,
+  );
+}
