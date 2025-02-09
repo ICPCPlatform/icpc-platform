294358c06473c6e41fb77cf743952a1961871ed7
Mohamed El tamawey <tammwy22@gmail.com>
2025-02-09 02:59:32 +0200

    Add: send email

---
+++ src/lib/email/sendEmail.ts
@@ -0,0 +1,24 @@
+import { Resend } from "resend";
+const resend = new Resend(process.env.RESEND_API_KEY);
+async function send({
+  to,
+  subject,
+  html,
+}: {
+  to: string[];
+  subject: string;
+  html: string;
+}) {
+  return await resend.emails.send({
+    from: "Icpc assiut Community <no-replay@icpcpassiut.tech>",
+    to,
+    subject,
+    html,
+  });
+}
+
+console.log(await send({
+  to: ["tammwy22@gmail.com"],
+  subject: "Hello",
+  html: "<h1>Hello</h1>",
+}))
