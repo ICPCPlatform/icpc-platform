30be87eaa2130d8be6846d4a05e6d21e9d8b7578
Mohamed El tamawey <tammwy22@gmail.com>
2025-02-09 03:51:35 +0200

    add: EmailAuth table
    EamilAuth that inclue token

---
+++ src/lib/db/schema/user/EmailAuth.ts
@@ -0,0 +1,8 @@
+import { sql } from "drizzle-orm";
+import { integer, pgTable, varchar , date} from "drizzle-orm/pg-core";
+
+
+export const EmailAuth = pgTable('email_auth', {
+  token: varchar().notNull(),
+  userId: integer().primaryKey(), 
+  expiresAt: date().unique().default(sql`now() + interval '7 day'`).notNull(), });
