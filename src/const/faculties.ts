a8a0afe7b98ff370941870cbe5a773ad27be4967
Sherif Elglaly <sherifelglaly@gmail.com>
2025-02-09 07:29:38 +0200

    feat(constants): add initial constants data
    Add faculties, departments, universities, country data and governorate codes

---
+++ src/const/faculties.ts
@@ -0,0 +1,24 @@
+const faculties = [
+    "Faculty of Engineering",
+    "Faculty of Medicine",
+    "Faculty of Commerce",
+    "Faculty of Science",
+    "Faculty of Law",
+    "Faculty of Computer and Artificial Intelligence",
+    "Faculty of Pharmacy",
+    "Faculty of Dentistry",
+    "Faculty of Nursing",
+    "Faculty of Physical Therapy",
+    "Faculty of Veterinary Medicine",
+    "Faculty of Arts",
+    "Faculty of Mass Communication",
+    "Faculty of Al-Alsun",
+    "Faculty of Economics and Political Science",
+    "Faculty of Fine Arts",
+    "Faculty of Applied Arts",
+    "Faculty of Tourism and Hotels",
+    "Faculty of Social Work",
+    "Faculty of Agriculture"
+] as const;
+
+export default faculties;
---
+++ src/const/universities.ts
@@ -0,0 +1,18 @@
+const universities =
+    ["Arab Academy for Science & Technology",
+        "Akhbar El Yom Academy", "Alexandria University",
+        "Arab Open University", "American University in Cairo",
+        "Assiut University", "Al Azhar University", "Beni Suef University",
+        "Benha University", "Cairo University", "Damanhour University", "Damietta University",
+        "El Shorouk Academy", "Fayoum University", "Future University", "German University in Cairo",
+        "Helwan University", "Higher Technological Institute", "Kafr El-Sheikh University", "Mansoura University",
+        "Menoufia University", "Minia University", "Misr International University", "Modern Acadmy",
+        "Modern Sciences & Arts University", "Military Technical College",
+        "Modern University For Technology and Information",
+        "Misr University for Sience and Technology", "Nile University", "October 6 university",
+        "Pharos International University", "Sadat Academy for Management Sciences", "Ain Shams University",
+        "Sohag University", "Sinai University", "Suez Canal University", "South Valley University",
+        "Tanta University", "Université Française d'Égypte", "Université Senghor d'Alexandrie", "Zagazig University",
+        "CIC - Canadian International College", "Deraya University", "Badr University in Cairo"] as const;
+
+export default universities;
