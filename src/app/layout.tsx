e11900c492310d92350ec7f5726b7c1587b3b7cb
Mohamed El tamawey <tammwy22@gmail.com>
2025-02-13 12:48:34 +0200

    update: edit userProvider
    add user provider to the base layout
    add useUserContext

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,10 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Navbar } from "@/components/NavBar";
-import { ThemeProvider } from 'next-themes';
+import { ThemeProvider } from "next-themes";
+import { decryptSession } from "@/lib/session";
+import { cookies } from "next/headers";
+import UserProvider from "@/providers/user";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,17 +14,22 @@ export const metadata: Metadata = {
   description: "A platform for managing ICPC-style programming competitions",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const cookieStore = await cookies();
+  const session = cookieStore.get("session")?.value;
+  const user = await decryptSession(session);
   return (
     <html lang="en">
       <body className={inter.className}>
         <ThemeProvider attribute="class">
-          <Navbar />
-          {children}
+          <UserProvider user={user}>
+            <Navbar />
+            {children}
+          </UserProvider>
         </ThemeProvider>
       </body>
     </html>
