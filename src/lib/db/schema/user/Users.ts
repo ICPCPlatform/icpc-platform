e4a705383267f2095967730ab7951061fe79d4c9
Mohamed Reda <mhassanien15@yahoo.com>
2025-02-17 10:15:13 +0200

    changes in user schema added vjhandle, removes isverified and last online, changed deleted to timestamp.

--- src/lib/db/schema/user/Users.ts
+++ src/lib/db/schema/user/Users.ts
@@ -1,10 +1,8 @@
 import {
   pgTable,
-  serial,
+  uuid,
   varchar,
   timestamp,
-  boolean,
-  char,
   uniqueIndex,
 } from "drizzle-orm/pg-core";
 
@@ -12,18 +10,21 @@ type Role = "admin" | "user" | "coach";
 export const Users = pgTable(
   "users",
   {
-    userId: serial().primaryKey(),
-    username: varchar().notNull().unique(),
-    password: varchar().notNull().unique(),
+    userId: uuid().primaryKey(),
+    username: varchar({ length: 24 }).notNull(),
+    password: varchar({ length: 75 }).notNull(),
 
-    gmail: varchar().notNull().unique(),
-    cfHandle: varchar().notNull().unique(),
-    phoneNumber: char({ length: 11 }).notNull().unique(),
+    gmail: varchar({ length: 40 }).notNull().unique(),
+    cfHandle: varchar({ length: 24 }).notNull().unique(),
+    vjHandle: varchar({ length: 16 }).unique(),
+
+    // international numbers could be accepted in the future
+    phoneNumber: varchar({ length: 15 }).notNull(),
     // set by admin and the server
     role: varchar({ length: 40 }).$type<Role>().default("user").notNull(),
-    lastOnline: timestamp().defaultNow(),
-    isVerified: boolean().default(false).notNull(),
-    deleted: boolean().default(false).notNull(),
+    deleted: timestamp(),// null means <not deleted> otherwise <time of deletion>
   },
-  (table) => [uniqueIndex("users_username_idx").on(table.username)],
+  // makes sure that the username is unique and searches are faster
+  (table) => [uniqueIndex("users_username_idx").on(table.username)] 
+  
 );
