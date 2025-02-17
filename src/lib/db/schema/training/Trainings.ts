25ebcab84427cbb49ad52b1eda6f3b3effea757d
Mohamed Reda <mhassanien15@yahoo.com>
2025-02-17 12:54:46 +0200

    updated Training to ERD3.0

--- src/lib/db/schema/training/Trainings.ts
+++ src/lib/db/schema/training/Trainings.ts
@@ -1,15 +1,35 @@
-import { integer, pgTable, varchar, date, serial } from "drizzle-orm/pg-core";
+import {
+  integer,
+  pgTable,
+  varchar,
+  date,
+  serial,
+  uuid,
+  boolean,
+  timestamp,
+} from "drizzle-orm/pg-core";
 
 import { Users } from "../user/Users";
+
 type Status = "active" | "inactive";
 export const Trainings = pgTable("trainings", {
   trainingId: serial().primaryKey(),
-  headId: integer()
-    .references(() => Users.userId)
+  headId: uuid()
+    .references(() => Users.userId, {
+      onDelete: "restrict",
+      onUpdate: "cascade",
+    })
     .notNull(),
-  title: varchar().notNull(),
-  description: varchar().notNull(),
+  chiefJudge: uuid()
+    .references(() => Users.userId, {
+      onDelete: "restrict",
+      onUpdate: "cascade",
+    })
+    .notNull(),
+  title: varchar({ length: 128 }).notNull().unique(),
+  description: varchar({ length: 512 }).notNull(),
   startDate: date().notNull(),
-  duration: integer(), // number of weeks/days
-  status: varchar({ length: 40 }).$type<Status>().notNull().default("active"),
+  duration: integer().notNull().default(1), // number of weeks/days
+  status: varchar({ length: 20 }).$type<Status>().notNull().default("active"),
+  deleted: timestamp(),
 });
