139a7c45be2a07a0a5cb7d1ced405f215ff1b5c2
Mohamed Reda <mhassanien15@yahoo.com>
2025-02-18 22:24:03 +0200

    modified session

--- src/lib/db/schema/training/Sessions.ts
+++ src/lib/db/schema/training/Sessions.ts
@@ -1,20 +1,29 @@
 import {
+  primaryKey,
   timestamp,
   integer,
   pgTable,
-  serial,
   varchar,
 } from "drizzle-orm/pg-core";
 
 import { Trainings } from "./Trainings";
 
-export const Sessions = pgTable("sessions", {
-  // 32 bits => (8 bits weeknumber + 24 bits pseudo-random number)
-  sessionId: integer().primaryKey(),
-  trainingId: serial()
-    .references(() => Trainings.trainingId)
-    .notNull(),
-  dateTime: timestamp().notNull(),
-  title: varchar().notNull(),
-  description: varchar().notNull(),
-});
+export const Sessions = pgTable(
+  "sessions",
+  {
+    trainingId: integer()
+      .references(() => Trainings.trainingId)
+      .notNull(),
+    weekNumber: integer().notNull(),
+    title: varchar({ length: 128 }).notNull(),
+    description: varchar({ length: 512 }).notNull(),
+    date: timestamp().notNull(),
+    deleted: timestamp(),
+  },
+  (table) => [
+    primaryKey({
+      name: "sessions_pkey",
+      columns: [table.weekNumber, table.trainingId],
+    }),
+  ]
+);
