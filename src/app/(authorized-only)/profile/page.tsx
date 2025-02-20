37bf5e3014f4c3b7e4b3e6fda5ed853f4795fe20
Mohamed El tamawey <tammwy22@gmail.com>
2025-02-20 09:01:46 +0200

    update: profile to match the new erd

--- src/app/(authorized-only)/profile/page.tsx
+++ src/app/(authorized-only)/profile/page.tsx
@@ -2,59 +2,19 @@ import "@/app/globals.css";
 
 import Sidebar from "../../../components/profile/_Sidebar";
 import Profile from "../../../components/profile/_Profile";
-import { UsersFullData } from "@/lib/db/schema/user/UsersFullData";
 import { cookies } from "next/headers";
 import { decryptSession } from "@/lib/session";
-import { db } from "@/lib/db";
-import { eq } from "drizzle-orm";
-import { Users } from "@/lib/db/schema/user/Users";
-import { Countries } from "@/lib/db/schema/user/Countries";
-import { Cities } from "@/lib/db/schema/user/Cities";
-import { Departments } from "@/lib/db/schema/user/Departments";
-import { Communities } from "@/lib/db/schema/user/Communities";
 import { redirect } from "next/navigation";
-import { Faculties } from "@/lib/db/schema/user/Faculties";
-import { Institutes } from "@/lib/db/schema/user/Institutes";
 import { getUserFullData } from "@/actions/getUserFullData";
+import { UserProfile } from "@/lib/types/userProfileType";
 
-export type User = {
-  username: string | null;
-  imageUrl: string | null;
-  gmail: string | null;
-  createdAt: Date | null;
-  country: string | null;
-  city: string | null;
-  institute: string | null;
-  department: string | null;
-  community: string | null;
-  faculty: string | null;
-  nameAR1: string | null;
-  nameAR2: string | null;
-  nameAR3: string | null;
-  nameAR4: string | null;
-  firstNameEn: string | null;
-  lastNameEn: string | null;
-  codeforces: string | null;
-  vjudge: string | null;
-  atcoder: string | null;
-  codechef: string | null;
-  leetcode: string | null;
-  cses: string | null;
-  facebook: string | null;
-  linkedIn: string | null;
-  twitter: string | null;
-  github: string | null;
-  telegram: string | null;
-  academicYear: number | null;
-  graduationDate: string | null;
-};
 export default async function ProfilePage() {
   const cookieStore = await cookies();
   const session = cookieStore.get("session")?.value;
   const validation = await decryptSession(session);
   if (!validation) return null;
   const { userId } = validation;
-  const user: User = await getUserFullData({ userId });
+  const user: UserProfile<false> | null = await getUserFullData({ userId });
   if (!user) redirect("/404");
 
   return (
