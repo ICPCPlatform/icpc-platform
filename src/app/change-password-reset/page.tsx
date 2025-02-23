3f17f2c6a933a96b71817ae49b999345ce139f16
Mohamed El tamawey <tammwy22@gmail.com>
2025-02-23 08:59:19 +0200

    imple: change-password-reset page

---
+++ src/app/change-password-reset/page.tsx
@@ -0,0 +1,52 @@
+import { db } from "@/lib/db";
+import { eq } from "drizzle-orm";
+import { ResetPassword } from "@/lib/db/schema/user/ResetPassword";
+import { Users } from "@/lib/db/schema/user/Users";
+import  bcrypt  from "bcrypt";
+import { redirect } from "next/navigation";
+
+export default async function Page({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | string[] | undefined };
+}) {
+  const token = searchParams.token as string;
+  const users = await db.select({ userId: ResetPassword.userId })
+    .from(ResetPassword)
+    .where(eq(ResetPassword.token, token))
+    .execute();
+  if (users.length === 0) {
+    redirect("/login");
+  }
+  
+  return (
+    <>
+      <h1>Change Password</h1>
+      <form action={handleSubmit}>
+        <input type="password" name="password" />
+        <input type="password" name="confirmPassword" />
+        <button type="submit">Change Password</button>
+      </form>
+    </>
+  );
+  async function handleSubmit(formData: FormData) {
+    const password = formData.get("password") as string;
+    const confirmPassword = formData.get("confirmPassword") as string;
+    if (password !== confirmPassword) {
+      alert("Passwords do not match");
+      return;
+    }
+    const [{userId}] = await db
+      .select({ userId: ResetPassword.userId })
+      .from(ResetPassword)
+      .where(eq(ResetPassword.token, token))
+      .execute();
+    if (!userId) {
+      console.log("Invalid token");
+    }
+    db.update(Users)
+      .set({ password: await bcrypt.hash(password, 10) })
+      .where(eq(Users.userId, userId))
+      .execute();
+  }
+}
