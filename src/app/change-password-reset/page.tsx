b087913c538ecf61b4190f1b329c18f0adc627ea
Mohamed El tamawey <tammwy22@gmail.com>
2025-03-03 23:26:36 +0200

    update:change-password-reset

--- src/app/change-password-reset/page.tsx
+++ src/app/change-password-reset/page.tsx
@@ -1,9 +1,8 @@
 import { db } from "@/lib/db";
 import { eq } from "drizzle-orm";
 import { ResetPassword } from "@/lib/db/schema/user/ResetPassword";
-import { Users } from "@/lib/db/schema/user/Users";
-import bcrypt from "bcrypt";
 import { redirect } from "next/navigation";
+import { FromReset } from "./_ResetPasswordForm";
 
 export default async function Page({
   searchParams,
@@ -19,49 +18,7 @@ export default async function Page({
   if (users.length === 0) {
     redirect("/login");
   }
-  async function handleSubmit(formData: FormData) {
-    "use server";
-    console.log("Change password");
-    const password = formData.get("password") as string;
-    const confirmPassword = formData.get("confirmPassword") as string;
-    const token = formData.get("token") as string;
-    if (password !== confirmPassword) {
-      console.log("Passwords do not match");
-      return;
-    }
-    console.log(token);
-    const  users  = await db
-      .select({ userId: ResetPassword.userId })
-      .from(ResetPassword)
-      .where(eq(ResetPassword.token, token))
-      .execute();
-    console.log(users);
-    if (!users) {
-      console.log("Invalid token");
-    }
-    if (users.length === 0) {
-      console.log("Invalid token");
-    }
-    const userId = users[0].userId;
-    await db.update(Users)
-      .set({ password: await bcrypt.hash(password, 10) })
-      .where(eq(Users.userId, userId))
-      .execute();
-    await db.delete(ResetPassword).where(eq(ResetPassword.userId, userId)).execute();
-    redirect("/login");
 
-    
-  }
-
-  return (
-    <>
-      <h1>Change Password</h1>
-      <form action={handleSubmit} method="post">
-        <input type="password" name="password"  required />
-        <input type="password" name="confirmPassword" required />
-        <input type="hidden" name="token" value={token} />
-        <button type="submit">Change Password</button>
-      </form>
-    </>
-  );
+  return <FromReset token={token} />;
 }
+
