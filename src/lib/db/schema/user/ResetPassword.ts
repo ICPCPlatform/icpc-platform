c222394ed9a3a165c1c3b8ccec3f5d6b915f5560
Mohamed El tamawey <tammwy22@gmail.com>
2025-03-03 21:47:39 +0200

    Add: endAt to ResetPassword

--- src/lib/db/schema/user/ResetPassword.ts
+++ src/lib/db/schema/user/ResetPassword.ts
@@ -1,12 +1,14 @@
-import { char, pgTable, uuid } from "drizzle-orm/pg-core";
+import { char, pgTable, timestamp, uuid } from "drizzle-orm/pg-core";
 import { Users } from "./Users";
+import { sql } from "drizzle-orm";
 
 
 export const ResetPassword = pgTable("reset_password", {
   userId: uuid().primaryKey().references(() => Users.userId, {
     onDelete: "cascade",
     onUpdate: "cascade"
-  }),
-  token: char({ length: 25 }),
+  }).notNull(),
+  token: char({ length: 25 }).notNull(),
+  endAt: timestamp().default(sql`now() + interval '1 day'`),
 
 })
