9ba79f5021ca57bdfdca1a1f4b36003bb33d9e65
Mohamed El tamawey <tammwy22@gmail.com>
2025-03-03 21:49:33 +0200

    update(ResetPassword): noDelete and on Update no action

--- src/lib/db/schema/user/ResetPassword.ts
+++ src/lib/db/schema/user/ResetPassword.ts
@@ -5,8 +5,8 @@ import { sql } from "drizzle-orm";
 
 export const ResetPassword = pgTable("reset_password", {
   userId: uuid().primaryKey().references(() => Users.userId, {
-    onDelete: "cascade",
-    onUpdate: "cascade"
+    onDelete: "no action",
+    onUpdate: "no action"
   }).notNull(),
   token: char({ length: 25 }).notNull(),
   endAt: timestamp().default(sql`now() + interval '1 day'`),
