e40f29255cc1bac17c9882b6d5a74028cbb3b5fd
Mohamed Reda <mhassanien15@yahoo.com>
2025-03-09 06:25:36 +0200

    modified schema Trainings.ts

--- src/lib/db/schema/training/Trainings.ts
+++ src/lib/db/schema/training/Trainings.ts
@@ -6,12 +6,15 @@ import {
   serial,
   uuid,
   timestamp,
+  json,
 } from "drizzle-orm/pg-core";
 
 import { Users } from "../user/Users";
 import { citext } from "@/lib/db/util";
-
+import {Materials} from "./Materials";
 type Status = "active" | "inactive";
+
+
 export const Trainings = pgTable("trainings", {
   // TODO make serial start from 1000
   trainingId: serial().primaryKey(),
@@ -29,6 +32,9 @@ export const Trainings = pgTable("trainings", {
     .notNull(),
   title: citext({ length: 128 }).notNull().unique(),
   description: varchar({ length: 512 }).notNull(),
+  materialId: integer().references(()=>Materials.id).notNull(),
+  standing: json(),
+  standingView: json(),// saves which attributes of standing are visible to trainees 
   startDate: date().notNull(),
   duration: integer().notNull().default(1), // number of weeks/days
   status: varchar({ length: 20 }).$type<Status>().notNull().default("active"),
