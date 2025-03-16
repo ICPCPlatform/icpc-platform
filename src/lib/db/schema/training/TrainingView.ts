ed4effb05609961d5121caff15b9cdca53425eca
Mohamed El tamawey <tammwy22@gmail.com>
2025-03-16 08:00:24 +0200

    update: refactor schema

--- src/lib/db/schema/training/TrainingView.ts
+++
@@ -1,8 +0,0 @@
-import { integer, jsonb, pgTable } from "drizzle-orm/pg-core";
-
-export const TrainingView = pgTable("training_view", {
-  trainingId: integer().primaryKey(),
-  data: jsonb()
-    .notNull()
-    .default("['name', 'handle', 'numberofsolved', 'mentor', 'level']"),
-});
