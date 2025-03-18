1aa802f00c6b0eaec2bc8f522a30b1984e87b126
Mohamed El tamawey <tammwy22@gmail.com>
2025-03-18 10:00:29 +0200

    import: simple style

--- src/app/protected/trainings/[trainingId]/__trainingNavigation.tsx
+++ src/app/protected/trainings/[trainingId]/__trainingNavigation.tsx
@@ -4,6 +4,9 @@ import { cn } from "@/lib/utils";
 import { useTrainingContext } from "@/providers/training";
 import Link from "next/link";
 
+
+
+const itemStyle = "w-full gap-3   text-sm font-medium rounded-md transition-colors whitespace-nowrap hover:bg-accent hover:text-accent-foreground my-2 box-border"
 export default function TrainingNavigation({
   trainingId,
 }: {
@@ -12,36 +15,48 @@ export default function TrainingNavigation({
   const training = useTrainingContext();
   console.log(training);
   return (
-    <nav
-      className={cn(
-        "rounded-lg",
-        "overflow-hidden",
-        "h-auto",
-        "self-center",
-        "my-10",
-        "mx-20"
-      )}
-      style={{ backgroundColor: "rgb(41, 41, 41)" }}
-    >
-      <ul className={cn("flex", "flex-col")}>
-        {training?.standing.map((context, i) => (
-          <li
-            key={i}
-            className={cn("p-5")}
+    <div className="flex gap-6">
+      <aside>
+        <div className="flex-shrink-0 w-64 bg-card rounded-lg border p-4">
+          <nav
+            className={cn(
+              "rounded-lg",
+              "overflow-hidden",
+              "h-auto",
+              "self-center",
+            )}
+            // style={{ backgroundColor: "rgb(41, 41, 41)" }}
           >
-            <Link
-              href={`/protected/trainings/${trainingId}/contests/${context.ContestInfo.id}/standing`}
-            >
-              {context.ContestInfo.title}
-            </Link>
-          </li>
-        ))}
-        <li className={cn("p-5", "bg-gray-500")}>
-          <Link href={`/protected/trainings/${trainingId}/material`}>
-            material
-          </Link>
-        </li>
-      </ul>
-    </nav>
+            <ul className={cn("flex", "flex-col")}>
+              {training?.standing.map((context, i) => (
+                <li
+                  key={i}
+                  className={cn(itemStyle,"bg-slate-400")}
+                >
+                  <Link
+                    href={`/protected/trainings/${trainingId}/contests/${context.ContestInfo.id}/standing`}
+                    className={cn("w-full block box-border px-4 py-2")}
+                  >
+                    contest: {context.ContestInfo.title}
+                  </Link>
+                </li>
+              ))}
+              <li
+                className={cn(itemStyle,
+                   "bg-zinc-500"
+                )}
+              >
+                <Link href={`/protected/trainings/${trainingId}/material`} className={cn("w-full block box-border px-4 py-2")}>
+                  material
+                </Link>
+              </li>
+              <li>
+
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </aside>
+    </div>
   );
 }
