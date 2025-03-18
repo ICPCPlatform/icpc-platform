cbf2898567e5893a6172685e8ae2fbfdec7a8069
Mohamed El tamawey <tammwy22@gmail.com>
2025-03-18 10:00:29 +0200

    import: simple style

--- src/app/protected/trainings/[trainingId]/layout.tsx
+++ src/app/protected/trainings/[trainingId]/layout.tsx
@@ -17,14 +17,15 @@ export default async function Layout({
   console.log(trainingData);
 
   return (
-
-    <div className={cn('w-max','flex','flex-row')}>
-      <TrainingProvider trainingData={trainingData}>
-        <TrainingNavigation trainingId={trainingId} />
-        <div>
-        <NoSSR>{children}</NoSSR>
-        </div>
-      </TrainingProvider>
+    <div className="mx-auto p-2 md:py-6">
+      <div className="flex flex-col lg:flex-row gap-4 md:gap-6 w-max max-w-full ">
+        <TrainingProvider trainingData={trainingData}>
+          <NoSSR>
+            <TrainingNavigation trainingId={trainingId} />
+            {children}
+          </NoSSR>
+        </TrainingProvider>
+      </div>
     </div>
   );
 }
