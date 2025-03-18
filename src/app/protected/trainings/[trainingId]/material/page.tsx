d28340b9c63d738958f287b38d5e841f19af418f
Mohamed El tamawey <tammwy22@gmail.com>
2025-03-18 08:45:00 +0200

    impl: basic material page

---
+++ src/app/protected/trainings/[trainingId]/material/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+import { useTrainingContext } from "@/providers/training";
+
+export default function Page() {
+  const training = useTrainingContext();
+  return <div>
+    <pre>{JSON.stringify(training?.material, null, 2)}</pre>
+  </div>;
+}
