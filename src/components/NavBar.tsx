a369be1b28bbaa430df9d1ec08a8f573c2e17ec1
Sherif Elglaly <sherifelglaly@gmail.com>
2025-04-15 20:08:15 +0200

    fix(navbar): correct training link href

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -58,7 +58,7 @@ export function Navbar() {
                     <li>
                       <NavigationMenuLink asChild>
                         <Link
-                          href="/protected/trainings"
+                          href="/protected/join-training"
                           className="block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground"
                         >
                           <div className="text-sm font-medium">
