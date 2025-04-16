de6d3529c657c6e733f21800c7f8f8ca3c16e397
Sherif Elglaly <sherifelglaly@gmail.com>
2025-04-16 18:35:18 +0200

    feat(contest): refine contest validation schema
    Refactor contest schema for improved validation and clarity.
    
    - Update validation rules for title, description, judge, and groupId.
    - Add error messages for each field to improve UX.
    - Rename `contestSchema` to `contestValidations` for clarity.

--- src/lib/validation/contestValidations.ts
+++ src/lib/validation/contestValidations.ts
@@ -1,17 +1,19 @@
+
+
 import { z } from 'zod';
+import { CONTEST_ERROR_MESSAGES } from '@/lib/const/contest-error-messages';
 
-
-export const contestSchema = z.object({
-    title: z.string().min(3, 'Name is required').max(100, 'Name is too long'),
-    description: z.string().min(5, 'Description is required').max(1000, 'Description is too long'),
-    judge: z.string().min(3, 'Judge is required').max(100, 'Judge is too long'),
-    type: z.enum(['individual', 'team'], {
-        errorMap: () => ({ message: 'Type is required' }),
-    }),
-    pointSpp: z.number().positive().min(1, 'Points per submission must be at least 1'),
-    firstPoints: z.number().positive().min(1, 'First points must be at least 1'),
-    calcSystem:z.number().positive().min(1, 'Calculation system must be at least 1'),
-    groupId: z.string().min(1, 'Group ID must be at least 1'),
+export const contestValidations = z.object({
+  groupId: z.string().min(1,CONTEST_ERROR_MESSAGES.GROUP_ID_REQUIRED).max(32, CONTEST_ERROR_MESSAGES.GROUP_ID_TOO_LONG).nullable(),
+  judge: z.string().min(1, CONTEST_ERROR_MESSAGES.JUDGE_REQUIRED).max(32, CONTEST_ERROR_MESSAGES.JUDGE_TOO_LONG),
+  title: z.string().min(1, CONTEST_ERROR_MESSAGES.TITLE_REQUIRED).max(128, CONTEST_ERROR_MESSAGES.TITLE_TOO_LONG),
+  description: z.string().min(1, CONTEST_ERROR_MESSAGES.DESCRIPTION_REQUIRED).max(512, CONTEST_ERROR_MESSAGES.DESCRIPTION_TOO_LONG),
+  type: z.enum(['practice', 'contest'], {
+    errorMap: () => ({ message: CONTEST_ERROR_MESSAGES.TYPE_INVALID }),
+  }).nullable(),
+  pointPerProblem: z.number().int().positive().min(1, CONTEST_ERROR_MESSAGES.POINTSPP_INVALID).default(20),
+  firstPoints: z.number().int().positive().min(1, CONTEST_ERROR_MESSAGES.FIRST_POINTS_INVALID).default(1000),
+  calcSys: z.string().min(1, CONTEST_ERROR_MESSAGES.CALC_SYSTEM_REQUIRED).max(32, CONTEST_ERROR_MESSAGES.CALC_SYSTEM_TOO_LONG).default('90%'),
 });
 
-export type ContestFormValues = z.infer<typeof contestSchema>;
+export type ContestFormValues = z.infer<typeof contestValidations>;
