7432fe6b1390e393f89591537ae4845edafcfafc
Mohamed Reda <mhassanien15@yahoo.com>
2025-04-26 21:37:38 +0300

    refactor: standardize import paths for Material and TrainingFullDTO types

---
+++ src/app/protected/trainings/[trainingId]/edit-materials/[blockId]/_action.ts
@@ -0,0 +1,46 @@
+"use server";
+import "server-only";
+import { db } from "@/lib/db";
+import { Blocks } from "@/lib/db/schema/training/Blocks";
+import { getUserTrainingPermissions } from "@/lib/permissions/getUserTrainingPermissions";
+import { UserDataJWT } from "@/lib/session";
+import { Material } from "@/lib/types/training";
+import { and, eq } from "drizzle-orm";
+
+export async function editMaterial({
+  trainingId,
+  blockNumber,
+  newMaterials,
+}: {
+  trainingId: number;
+  blockNumber: number;
+  newMaterials: Material[];
+}) {
+  const userData = await getUserData();
+  if (!userData) {
+    return;
+  }
+  const userId = userData.userId;
+  if (isNaN(trainingId)) {
+    return;
+  }
+  const userPermissions = await getUserTrainingPermissions(userId, trainingId);
+
+  if (!userPermissions.includes("Edit:material")) {
+    return;
+  }
+  
+
+  await db
+    .update(Blocks)
+    .set({
+      material: newMaterials,
+      // material: newMaterial,
+    })
+    .where(
+      and(
+        eq(Blocks.blockNumber, blockNumber),
+        eq(Blocks.trainingId, trainingId),
+      ),
+    );
+}
