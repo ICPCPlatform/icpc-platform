f8b6206d279fec7d352f5af16677e3d16c589e8c
Sherif Elglaly <sherifelglaly@gmail.com>
2025-04-27 20:58:09 +0300

    fix(training-blocks): use getUserData for user ID
    Use `getUserData` instead of headers for user ID retrieval.

--- src/app/protected/trainings/[trainingId]/blocks/actions.ts
+++ src/app/protected/trainings/[trainingId]/blocks/actions.ts
@@ -3,7 +3,7 @@ import {cache} from "react";
 import {db} from "@/lib/db";
 import {and, eq} from "drizzle-orm";
 import {Blocks} from "@/lib/db/schema/training/Blocks";
-import {UserDataJWT} from "@/lib/session";
+import {getUserData, UserDataJWT} from "@/lib/session";
 import {getUserTrainingPermissions} from "@/lib/permissions/getUserTrainingPermissions";
 import {blockValidations} from "@/lib/validation/blockValidations";
 import { z } from "zod";
@@ -25,20 +25,15 @@ export const getBlock = cache(async (blockNumber: number) => {
 type FormData = z.infer<typeof blockValidations>;
 export default async function updateBlock( {trainingId, blockNumber, ...data}: FormData & {trainingId: number;blockNumber: number }) {
     const { title, description } = data;
-
-
-    // check user login and permissions
-    const headers = new Headers();
     try {
-        const { userId } = JSON.parse(
-            headers.get("x-user") ?? "{'userId':''}",
-        ) as UserDataJWT ;
-
-        if (!userId) {
-            console.log("User ID not found in headers");
+        // TODO : does not work with JWT
+        const user = await getUserData();
+        if (!user) {
+            console.log("User not found in headers");
             redirect("/login");
+            return
         }
-        const userPermissions = await getUserTrainingPermissions(userId, Number(trainingId));
+        const userPermissions = await getUserTrainingPermissions(user.userId, Number(trainingId));
         if (!userPermissions.includes("Edit:block")) {
             return;
         }
