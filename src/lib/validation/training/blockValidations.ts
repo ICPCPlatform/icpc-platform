06a46baf7699d99a38527966ecd8b6e880faf372
Sherif Elglaly <sherifelglaly@gmail.com>
2025-05-03 23:29:35 +0300

    feat(training): add block validations

---
+++ src/lib/validation/training/blockValidations.ts
@@ -0,0 +1,9 @@
+import zod from 'zod';
+import { BLOCK_ERROR_MESSAGES } from '@/lib/const/block-error-messages';
+export const blockValidations = zod.object({
+    title: zod.string().min(1, BLOCK_ERROR_MESSAGES.TITLE_REQUIRED).max(128, BLOCK_ERROR_MESSAGES.TITLE_TOO_LONG),
+    description: zod.string().min(1, BLOCK_ERROR_MESSAGES.DESCRIPTION_REQUIRED).max(512, BLOCK_ERROR_MESSAGES.DESCRIPTION_TOO_LONG),
+// TODO: add more validations hidden
+})
+
+export type BlockFormData = zod.infer<typeof blockValidations>
