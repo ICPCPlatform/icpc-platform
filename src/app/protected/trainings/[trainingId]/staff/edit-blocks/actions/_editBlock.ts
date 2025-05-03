551e5487f45d370f29123a4b4bf1befb39e82435
Sherif Elglaly <sherifelglaly@gmail.com>
2025-05-04 02:22:53 +0300

    feat(edit-blocks): implement block update and delete
    Implements update and delete functionality for training blocks.

--- src/app/protected/trainings/[trainingId]/staff/edit-blocks/actions/_editBlock.ts
+++ src/app/protected/trainings/[trainingId]/staff/edit-blocks/actions/_editBlock.ts
@@ -7,7 +7,11 @@ import { Blocks } from "@/lib/db/schema/training/Blocks";
 import { staffViewBlock } from "@/lib/types/staff/StaffTrainingTypes";
 import {BlockFormData} from "@/lib/validation/training/blockValidations";
 
-
+/**
+ * Fetches all blocks for a given training ID.
+ * @param trainingId - The ID of the training.
+ * @Returns {Promise<staffViewBlock[] | null>} - Returns an array of blocks or null if an error occurs.
+ */
 export const getAllBlocks = cache(async (trainingId: number) => {
     try {
         // Fetch user data
@@ -33,18 +37,20 @@ export const getAllBlocks = cache(async (trainingId: number) => {
         }
 
         // Fetch all blocks from the database
-        const resultBlocks = await db
-            .select({
-                blockNumber: Blocks.blockNumber,
-                trainingId: Blocks.trainingId,
-                title: Blocks.title,
-                description: Blocks.description,
-            })
+        const resultBlocks : staffViewBlock[] = await db
+            .select(
+                {
+                    blockNumber: Blocks.blockNumber,
+                    title: Blocks.title,
+                    description: Blocks.description,
+                    trainingId: Blocks.trainingId,
+                }
+            )
             .from(Blocks)
             .where(eq(Blocks.trainingId, trainingId))
             .execute();
 
-        return resultBlocks as staffViewBlock[];
+        return resultBlocks;
     } catch (error) {
         console.error("Error fetching blocks:", error);
         return null;
@@ -52,6 +58,11 @@ export const getAllBlocks = cache(async (trainingId: number) => {
 });
 
 
+/**
+ * Fetches user permissions for editing blocks in a training.
+ * @param trainingId - The ID of the training.
+ * @returns {Promise<boolean | null>} - Returns true if the user has permission to edit blocks, false otherwise.
+ */
 export const getUserEditBlockPermissions = cache(async (trainingId: number) => {
     try {
 
@@ -82,7 +93,15 @@ export const getUserEditBlockPermissions = cache(async (trainingId: number) => {
 });
 
 
-export default async function updateBlock ({trainingId, blockNumber, ...data} : BlockFormData &{trainingId : number,blockNumber:number}) {
+/**
+ * Updates a block in the database.
+ * @param trainingId - The ID of the training.
+ * @param blockNumber - The block number to update.
+ * @param data - The block data to update (title and description).
+ *
+ * @Returns {Promise<void | null>} - Returns null if an error occurs.
+ */
+export async function updateBlock ({trainingId, blockNumber, ...data} : BlockFormData &{trainingId : number,blockNumber:number}) {
     const { title, description } = data;
     try {
         const userPermissions = await getUserEditBlockPermissions(trainingId);
@@ -102,12 +121,48 @@ export default async function updateBlock ({trainingId, blockNumber, ...data} : 
                   eq(Blocks.trainingId, trainingId),
                   eq(Blocks.blockNumber, blockNumber),
               )
-            )
-            .returning()
-            .execute();
+            ).execute();
+
+        // got to the edit-blocks page
+        window.location.href = `/protected/trainings/${trainingId}/staff/edit-blocks`;
     }
     catch (error) {
         console.error("Error updating block:", error);
         return null;
     }
+}
+
+export async function deleteBlock ({trainingId, blockNumber} : {trainingId : number,blockNumber:number}) {
+    try {
+        const userPermissions = await getUserEditBlockPermissions(trainingId);
+        if (!userPermissions) {
+            console.error("User does not have permission to edit blocks");
+            return null;
+        }
+        // Update block in the database
+        await db
+            .update(Blocks)
+            .set({
+                deleted: new Date(),
+            })
+            .where(
+              and(
+                  eq(Blocks.trainingId, trainingId),
+                  eq(Blocks.blockNumber, blockNumber),
+              )
+            )
+            .returning()
+            .execute();
+        // got to the edit-blocks page
+        window.location.href = `/protected/trainings/${trainingId}/staff/edit-blocks`;
+    }
+    catch (error) {
+        console.error("Error updating block:", error);
+        return null;
+    }
+}
+
+
+export async function createBlock () {
+    //TODO: To be implemented
 }
