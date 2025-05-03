ffe7a7fc38956739b793a00e1b26cc5970a09284
Sherif Elglaly <sherifelglaly@gmail.com>
2025-05-03 23:29:07 +0300

    feat(block-editor): add block error messages

---
+++ src/lib/const/block-error-messages.ts
@@ -0,0 +1,7 @@
+export const BLOCK_ERROR_MESSAGES = {
+    TITLE_REQUIRED: 'Title is required',
+    TITLE_TOO_LONG: 'Title is too long',
+    DESCRIPTION_REQUIRED: 'Description is required',
+    DESCRIPTION_TOO_LONG: 'Description is too long',
+
+} as const;
