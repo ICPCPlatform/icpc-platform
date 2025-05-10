20d9394205f180159cd6e7ed33a548364401b1f7
Sherif Elglaly <sherifelglaly@gmail.com>
2025-05-10 04:37:58 +0300

    feat(edit-blocks): add edit block button

---
+++ src/app/protected/trainings/[trainingId]/staff/edit-blocks/_editBlockButton.tsx
@@ -0,0 +1,33 @@
+"use client"
+import {Button} from "@/components/ui/button";
+import "@/styles/components/block.css";
+import {PencilIcon} from "lucide-react";
+import {useRouter} from "next/navigation";
+import {useState} from "react";
+
+export default function EditBlockButton(params: Readonly<{
+    trainingId: number;
+    blockNumber: number;
+}>) {
+    const router = useRouter();
+    const [isSubmitting, setIsSubmitting] = useState(false);
+
+    const handleEditBlock = () => {
+        setIsSubmitting(true);
+        // return to the update block page
+        router.push(`/protected/trainings/${params.trainingId}/staff/edit-blocks/${params.blockNumber}`);
+
+    };
+
+    return (
+        <Button
+            variant="outline"
+            className="edit-block-button"
+            onClick={handleEditBlock}
+            disabled={isSubmitting}
+        >
+            <PencilIcon className="h-4 w-4 mr-2"/>
+            Edit Block
+        </Button>
+    );
+}
