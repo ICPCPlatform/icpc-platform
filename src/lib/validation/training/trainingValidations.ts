2479c3a165a12adbfa6ad8dab50d4d9dc34319f6
Sherif Elglaly <sherifelglaly@gmail.com>
2025-05-19 12:31:20 +0300

    feat(training): add 'private' status to trainingValidations

--- src/lib/validation/training/trainingValidations.ts
+++ src/lib/validation/training/trainingValidations.ts
@@ -2,11 +2,22 @@ import zod from 'zod';
 import { TRAINING_ERROR_MESSAGES } from '@/lib/const/training-error-messages';
 
 export const trainingValidations = zod.object({
-    title: zod.string().min(5, TRAINING_ERROR_MESSAGES.TITLE_REQUIRED).max(128, TRAINING_ERROR_MESSAGES.TITLE_TOO_LONG),
-    description: zod.string().min(15, TRAINING_ERROR_MESSAGES.DESCRIPTION_REQUIRED).max(512, TRAINING_ERROR_MESSAGES.DESCRIPTION_TOO_LONG),
-    startDate: zod.date().min(new Date(), TRAINING_ERROR_MESSAGES.START_DATE_INVALID),
-    duration: zod.number().positive(TRAINING_ERROR_MESSAGES.DURATION_INVALID),
-    status: zod.enum(['active', 'roadmap', 'planned', 'completed']),
+    title: zod.string()
+        .min(5, TRAINING_ERROR_MESSAGES.TITLE_REQUIRED)
+        .max(128, TRAINING_ERROR_MESSAGES.TITLE_TOO_LONG),
+
+    description: zod.string()
+        .min(15, TRAINING_ERROR_MESSAGES.DESCRIPTION_REQUIRED)
+        .max(512, TRAINING_ERROR_MESSAGES.DESCRIPTION_TOO_LONG),
+
+    startDate: zod.date()
+        .min(new Date(), TRAINING_ERROR_MESSAGES.START_DATE_INVALID),
+
+    duration: zod.number()
+        .positive(TRAINING_ERROR_MESSAGES.DURATION_INVALID),
+
+    status: zod.enum(['active', 'roadmap', 'planned', 'completed', 'private'])
+        .nullable(),
 });
 
 export type TrainingFormData = zod.infer<typeof trainingValidations>;
