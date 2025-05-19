d884c086598da7e0e019e2215bd1510f8e8c3bb7
Sherif Elglaly <sherifelglaly@gmail.com>
2025-05-19 16:18:08 +0300

    feat(training): add title and description length validation

--- src/lib/const/training-error-messages.ts
+++ src/lib/const/training-error-messages.ts
@@ -1,7 +1,9 @@
 export const TRAINING_ERROR_MESSAGES = {
     TITLE_REQUIRED: 'Title is required',
+    TITLE_TOO_SHORT: 'Title must be more than 3 characters',
     TITLE_TOO_LONG: 'Title must be less than 128 characters',
     DESCRIPTION_REQUIRED: 'Description is required',
+    DESCRIPTION_TOO_SHORT: 'Description must be more than 3 characters',
     DESCRIPTION_TOO_LONG: 'Description must be less than 512 characters',
     START_DATE_REQUIRED: 'Start date is required',
     START_DATE_INVALID: 'Start date must be in the future',
