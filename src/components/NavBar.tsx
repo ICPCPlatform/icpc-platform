3ba8d61ec4de686224b623ff2cc3063415925d8c
Omar Abdelrahman Abbas <omar.abbas.7001@gmail.com>
2025-06-25 03:46:48 +0300

    fix(NavBar): update link for my trainings page
    - Changes the href for the my trainings link from "/protected/my-trainings" to "/protected/trainings/my-trainings" for correct routing.

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -73,7 +73,7 @@ export function Navbar() {
                     <li>
                       <NavigationMenuLink asChild>
                         <Link
-                          href="/protected/my-trainings"
+                          href="/protected/trainings/my-trainings"
                           className="block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground"
                         >
                           <div className="text-sm font-medium">
