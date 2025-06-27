838a66dff4eb13bf5cfc4b2270f83ee5334dcc98
Mohamed El tamawey <tammwy22@gmail.com>
2025-06-27 07:25:34 +0300

    Revert "fix: staff managment"
    This reverts commit 5869e08056557c6d1ca8984c975919b5efce2e80.

--- src/components/training/TrainingNavigation.tsx
+++ src/components/training/TrainingNavigation.tsx
@@ -32,7 +32,7 @@ export async function TrainingNavigation({
     { perm: "View:standing", href: `/protected/trainings/${trainingId}/leaderboard`, label: "Leaderboard" },
     { perm: "Edit:staff", href: `/protected/trainings/${trainingId}/staff/edit-training/add-staff`, label: "Manage Staff" },
     { perm: "Edit:training", href: `/protected/trainings/${trainingId}/staff/edit-training`, label: "Edit Training" },
-    { perm: "Edit:staff", href: `/protected/trainings/${trainingId}/staff/staff-management`, label: "Staff Management" },
+    { perm: "Edit:block", href: `/protected/trainings/${trainingId}/staff/edit-blocks`, label: "Edit Blocks" },
     { perm: "Edit:standing", href: `/protected/trainings/${trainingId}/staff/edit-standing-view`, label: "Standing View" },
   ];
 
