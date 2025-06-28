244ea007c0e3f980e995a65694484d027bc38053
Omar Abdelrahman Abbas <omar.abbas.7001@gmail.com>
2025-06-28 08:36:56 +0300

    fix: update default user handles for consistency

--- src/devtools/seedUsers.ts
+++ src/devtools/seedUsers.ts
@@ -18,8 +18,8 @@ export async function ensureDefaultUsers(): Promise<SeedUserStatus[]> {
       gmail: "admin_icpc@gmail.com",
       password: "Admin@123",
       role: "admin",
-      cfHandle: "admin_cf",
-      vjHandle: "admin_vj",
+      cfHandle: "IcpcPlatform",
+      vjHandle: "icpcPlatform",
       phoneNumber: "+2010000000000",
     },
     {
@@ -27,8 +27,8 @@ export async function ensureDefaultUsers(): Promise<SeedUserStatus[]> {
       gmail: "user_icpc@gmail.com",
       password: "User@1234",
       role: "user",
-      cfHandle: "user_cf",
-      vjHandle: "user_vj",
+      cfHandle: "AmeerJoy",
+      vjHandle: "AmeerJoy",
       phoneNumber: "+2010000000001",
     },
   ];
