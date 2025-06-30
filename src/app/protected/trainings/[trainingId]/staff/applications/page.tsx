b2c21f8eb3620b3cfc024be75f603b9f5533f19d
Mohamed El tamawey <tammwy22@gmail.com>
2025-06-30 07:10:08 +0300

    refactor: restructure handleBulkAction to use a single input schema and improve readability

--- src/app/protected/trainings/[trainingId]/staff/applications/page.tsx
+++ src/app/protected/trainings/[trainingId]/staff/applications/page.tsx
@@ -2,6 +2,7 @@ import { db } from "@/lib/db";
 import { Applications } from "@/lib/db/schema/training/Applications";
 import { Users } from "@/lib/db/schema/user/Users";
 import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
+import { eq } from "drizzle-orm";
 import ApplicationsTable from "./_ApplicationsTable";
 
 export default async function ApplicationsManagementPage({
