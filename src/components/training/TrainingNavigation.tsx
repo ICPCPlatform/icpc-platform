1a6cd36533defe586f6183a016817d9b1fa03942
Omar Abdelrahman Abbas <omar.abbas.7001@gmail.com>
2025-07-01 21:41:48 +0300

    refactor: remove async from TrainingNavigation component for cleaner implementation

--- src/components/training/TrainingNavigation.tsx
+++ src/components/training/TrainingNavigation.tsx
@@ -5,7 +5,7 @@ import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 import { Home, BookOpen, Trophy, Users } from "lucide-react";
 
-export async function TrainingNavigation({
+export function TrainingNavigation({
   trainingId,
   className = "",
 }: {
